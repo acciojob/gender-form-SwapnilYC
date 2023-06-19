49d908d47eeee9735890cb1828aef43f4eaecf2a
SwapnilYC <119727525+SwapnilYC@users.noreply.github.com>
2023-06-19 06:14:30 +0000

    radio button name and id changed

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 import './../styles/App.css';
 
-const App = () => {
+export default function App() {
   let [gender, setGender] = useState("");
 
   function genderHandler(gen) {
@@ -12,39 +12,47 @@ const App = () => {
   return (
     <div>
       <h1>Select your gender:</h1>
-      <input onClick={() => genderHandler("Male")} type="radio" id="male" name="male" value="Male" />
+      <input
+        onClick={() => genderHandler("Male")}
+        type="radio"
+        id="male"
+        name="gender"
+        value="Male"
+      />
       <h2 for="male">Male</h2>
-      <input onClick={() => genderHandler("Female")} type="radio" id="female" name="female" value="Female" />
+      <input
+        onClick={() => genderHandler("Female")}
+        type="radio"
+        id="female"
+        name="gender"
+        value="Female"
+      />
       <h2 for="female">Female</h2>
       {/* Do not remove the main div */}
 
-      {
-        gender === "Male" ? (
-          <>
-            <label for="shirts">Select your shirt size:</label>
-            <select name="shirtForMale" id="shirts">
-              <option value="select_size">Select size</option>
-              <option value="small">Small</option>
-              <option value="medium">Medium</option>
-              <option value="large">Large</option>
-            </select>
-          </>
-
-        ) : (
-          <>
-            <label for="dress">Select your dress size:</label>
-            <select name="shirtForFemale" id="dress">
-              <option value="select_size">Select size</option>
-              <option value="2">2</option>
-              <option value="4">4</option>
-              <option value="6">6</option>
-            </select>
-          </>
-
-        )
-      }
+      {gender === "Male" ? (
+        <>
+          <label for="shirts">Select your shirt size:</label>
+          <select name="shirtForMale" id="shirts">
+            <option value="select_size">Select size</option>
+            <option value="small">Small</option>
+            <option value="medium">Medium</option>
+            <option value="large">Large</option>
+          </select>
+        </>
+      ) : (
+        <>
+          <label for="dress">Select your dress size:</label>
+          <select name="shirtForFemale" id="dress">
+            <option value="select_size">Select size</option>
+            <option value="2">2</option>
+            <option value="4">4</option>
+            <option value="6">6</option>
+          </select>
+        </>
+      )}
     </div>
-  )
+  );
 }
 
 // const App = () => {
@@ -55,4 +63,4 @@ const App = () => {
 //   );
 // }
 
-export default App
+// export default App
